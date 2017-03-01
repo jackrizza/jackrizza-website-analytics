503c5682a924ef0f05ae3658f6436f3b430a22d3
jackrizza <jackrizza@gmail.com>
2017-02-28 21:31:39 -0500

    better data handler

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ setTimeout( () => {
         // 
         //do everything in here
         //
-        data.statistics.filter( (a) => {
+        data._statsGetter().filter( (a) => {
                 a = JSON.parse(a).time;
                 a = a.split(' ')[0];
                 if(timeArray.includes(a)) {
